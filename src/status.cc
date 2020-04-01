2e0dd66faae1580450ae13e1ba13822ec9cf2bbd
mxs <mxs@sbrk.org>
2020-04-01 08:48:10 +0100

    feat: add support of status code

---
+++ src/status.cc
@@ -0,0 +1,60 @@
+#include <ostream>
+
+#include "status.h"
+
+namespace bt {
+
+Status::Status() : code_(StatusCode::OK) {}
+
+Status::Status(StatusCode code, const std::string &message)
+    : code_(code), message_(message) {}
+
+Status::Status(StatusCode code) : code_(code) {}
+
+Status::Status(const Status &status)
+    : code_(status.code_), message_(status.message_) {}
+
+Status &Status::operator=(const Status &status) {
+  if (this != &status) {
+    code_ = status.code_;
+    message_ = status.message_;
+  }
+  return *this;
+}
+
+bool Status::operator==(StatusCode code) const { return code_ == code; }
+
+bool Status::operator!=(StatusCode code) const { return !operator==(code); }
+
+StatusCode Status::Code() const { return code_; }
+
+const std::string &Status::Message() const { return message_; }
+
+std::ostream &operator<<(std::ostream &os, const Status &status) {
+  std::string str_code;
+
+  switch (status.Code()) {
+  case OK:
+    str_code = "OK";
+    break;
+  case INTERNAL_ERROR:
+    str_code = "INTERNAL_ERROR";
+    break;
+  default:
+    str_code = "UNKNOWN";
+    break;
+  }
+
+  if (status == StatusCode::OK) {
+    os << str_code;
+  } else {
+    os << str_code;
+    if (!status.Message().empty()) {
+      os << " (" << status.Message() << ")";
+    }
+  }
+
+  return os;
+}
+
+} // namespace bt
---
+++ src/status.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <cassert>
+#include <iosfwd>
+#include <sstream>
+#include <string>
+
+namespace bt {
+
+enum StatusCode {
+  OK = 0,
+  INTERNAL_ERROR,
+};
+
+class Status {
+public:
+  Status();
+  Status(StatusCode code, const std::string &message);
+  Status(StatusCode code);
+  Status(const Status &status);
+
+  Status &operator=(const Status &status);
+  bool operator==(StatusCode code) const;
+  bool operator!=(StatusCode code) const;
+
+  StatusCode Code() const;
+  const std::string &Message() const;
+  std::string &Message();
+
+private:
+  StatusCode code_;
+  std::string message_;
+};
+
+template <typename T> class StatusOr {
+public:
+  StatusOr() : status_(StatusCode::INTERNAL_ERROR), value_() {}
+  StatusOr(StatusCode code) : status_(code), value_() {}
+  StatusOr(const Status &status) : status_(status), value_() {}
+  StatusOr(const T &value) : status_(StatusCode::OK), value_(value) {}
+  StatusOr(T &&value) : status_(StatusCode::OK), value_(std::move(value)) {}
+
+  const Status &GetStatus() const { return status_; }
+
+  bool Ok() const { return status_ == StatusCode::OK; }
+
+  T &ValueOrDie() {
+    assert(status_ == StatusCode::OK);
+    return value_;
+  }
+
+private:
+  Status status_;
+  T value_;
+};
+
+std::ostream &operator<<(std::ostream &os, const Status &status);
+
+Status &operator<<(Status &status, std::ostream &os);
+
+#define RETURN_IF_ERROR(status)                                                \
+  do {                                                                         \
+    if (status != StatusCode::OK) {                                            \
+      return status;                                                           \
+    }                                                                          \
+  } while (false)
+
+#define ASSIGN_OR_RETURN(what, expr)                                           \
+  do {                                                                         \
+    auto __status_or = expr;                                                   \
+    what = __status_or.ValueOrDie();                                           \
+    RETURN_IF_ERROR(__status_or.GetStatus());                                  \
+  } while (false)
+
+#define MOVE_OR_RETURN(what, expr)                                             \
+  do {                                                                         \
+    auto __status_or = expr;                                                   \
+    RETURN_IF_ERROR(__status_or.GetStatus());                                  \
+    what = std::move(__status_or.ValueOrDie());                                \
+  } while (false)
+
+#define RETURN_ERROR(code, what)                                               \
+  do {                                                                         \
+    std::stringstream message;                                                 \
+    message << what;                                                           \
+    return Status(code, message.str());                                        \
+  } while (false)
+
+} // namespace bt
