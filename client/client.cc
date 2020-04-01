d06166b6751bc1c2a6b00663006442059a7daebe
mxs <mxs@sbrk.org>
2020-04-01 22:33:53 +0100

    feat: client can now talk via gRPC to the server

--- client/client.cc
+++ client/client.cc
@@ -2,18 +2,68 @@
 #include <glog/logging.h>
 #include <grpcpp/grpcpp.h>
 
-namespace bt {
+#include "client/client.h"
+
+using namespace bt;
 
 namespace {
 constexpr char kServerAddress[] = "0.0.0.0:6000";
 } // anonymous namespace
 
-} // namespace bt
+Status Client::Init() {
+  stub_ = backtracer::Pusher::NewStub(
+      grpc::CreateChannel(kServerAddress, grpc::InsecureChannelCredentials()));
+  return StatusCode::OK;
+}
+
+Status Client::Run() {
+  switch (mode_) {
+  case NONE:
+    break;
+  case BATCH_PUSH:
+    return BatchPush();
+  };
+
+  return StatusCode::OK;
+}
+
+Status Client::BatchPush() {
+  backtracer::PutLocationRequest request;
+  backtracer::PutLocationResponse response;
+  request.set_user_id(42);
+  for (int i = 0; i < 10000; ++i) {
+    grpc::ClientContext context;
+    grpc::Status status = stub_->PutLocation(&context, request, &response);
+    if (!status.ok()) {
+      RETURN_ERROR(INTERNAL_ERROR,
+                   "unable to send location to backtracer, status="
+                       << status.error_message());
+    }
+  }
+
+  return StatusCode::OK;
+}
 
 int main(int ac, char **av) {
   FLAGS_logtostderr = 1;
   ::google::InitGoogleLogging(av[0]);
   ::gflags::ParseCommandLineFlags(&ac, &av, true);
+
+  Client client;
+  Status status = client.Init();
+
+  if (status != StatusCode::OK) {
+    LOG(WARNING) << "unable to init client, status=" << status;
+    return -1;
+  }
+
+  status = client.Run();
+  if (status != StatusCode::OK) {
+    LOG(WARNING) << "client exiting with error, status=" << status;
+    return -1;
+  }
+
+  LOG(INFO) << "client did its job";
 
   return 0;
 }
---
+++ client/client.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+
+#include "common/status.h"
+#include "proto/backtrace.grpc.pb.h"
+
+namespace bt {
+
+// A gRPC client that can be used to push or retrieve points from the
+// database. Multiple modes are supported and they can be used
+// concurrently.
+class Client {
+public:
+  // Available modes.
+  enum Mode {
+    NONE = 0,   // Do nothing.
+    BATCH_PUSH, // Simulates a client pushing points in batches.
+  };
+
+  Status Init();
+  Status Run();
+
+private:
+  Status BatchPush();
+
+  // TODO: Configure this via a flag.
+  Mode mode_ = BATCH_PUSH;
+  std::unique_ptr<backtracer::Pusher::Stub> stub_;
+};
+
+} // namespace bt
