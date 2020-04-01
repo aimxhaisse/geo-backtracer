f0ffbbf8a63c7be7426c457b28f40c4157168812
mxs <mxs@sbrk.org>
2020-04-01 09:49:25 +0100

    feat: add unit tests for status

---
+++ src/status_test.cc
@@ -0,0 +1,44 @@
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "status.h"
+
+namespace bt {
+namespace {
+
+TEST(StatusTest, Default) {
+  Status status;
+
+  EXPECT_EQ(status, StatusCode::OK);
+}
+
+TEST(StatusTest, Reuse) {
+  Status status;
+
+  status = StatusCode::INTERNAL_ERROR;
+
+  EXPECT_EQ(status, StatusCode::INTERNAL_ERROR);
+}
+
+TEST(StatusOr, Default) {
+  StatusOr<int> status;
+
+  EXPECT_FALSE(status.Ok());
+}
+
+TEST(StatusOr, Ok) {
+  StatusOr<int> status(42);
+
+  EXPECT_TRUE(status.Ok());
+  EXPECT_EQ(status.ValueOrDie(), 42);
+}
+
+TEST(StatusOr, Ko) {
+  StatusOr<int> status(StatusCode::INTERNAL_ERROR);
+
+  EXPECT_FALSE(status.Ok());
+}
+
+} // namespace
+
+} // namespace bt
