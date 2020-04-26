62b7f0a01e1626ad0de67cf6e33a36c35029ea31
mxs <mxs@sbrk.org>
2020-04-26 10:35:57 +0200

    feat: add enum to find out if a location is near a zone

--- server/zones.h
+++ server/zones.h
@@ -9,6 +9,12 @@ namespace bt {
 
 // Tools to manipulate zones.
 
+enum LocIsNearZone {
+  NONE = 0,     // This location is not near any other zones
+  PREVIOUS = 1, // This location is adjacent to the previous zone
+  NEXT = 2,     // This location is adjacent to the next zone
+};
+
 inline float GPSLocationToGPSZone(float gps_location) {
   return roundf(gps_location * kGPSZonePrecision) / kGPSZonePrecision;
 }
