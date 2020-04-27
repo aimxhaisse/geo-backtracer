ff6bedbca43afef11d0bb4763ef374751daf825d
mxs <mxs@sbrk.org>
2020-04-27 12:40:59 +0200

    feat: add large unit test for delete user API

--- server/pusher_test.cc
+++ server/pusher_test.cc
@@ -39,5 +39,57 @@ TEST_F(PusherTest, DeleteUserSimpleOK) {
   }
 }
 
+TEST_F(PusherTest, DeleteUserSimpleKO) {
+  EXPECT_EQ(server_->Init(options_), StatusCode::OK);
+
+  EXPECT_TRUE(PushPoint(kBaseTimestamp, kBaseUserId, kBaseGpsLongitude,
+                        kBaseGpsLatitude, kBaseGpsAltitude));
+
+  {
+    proto::GetUserTimelineResponse response;
+    EXPECT_TRUE(FetchTimeline(kBaseUserId, &response));
+    EXPECT_EQ(response.point_size(), 1);
+  }
+
+  EXPECT_TRUE(DeleteUser(kBaseUserId + 2));
+
+  {
+    proto::GetUserTimelineResponse response;
+    EXPECT_TRUE(FetchTimeline(kBaseUserId, &response));
+    EXPECT_EQ(response.point_size(), 1);
+  }
+}
+
+TEST_F(PusherTest, DeleteUserLargeOK) {
+  EXPECT_EQ(server_->Init(options_), StatusCode::OK);
+
+  for (int i = 0; i < 10000; ++i) {
+    for (int j = 0; j < 42; ++j) {
+      EXPECT_TRUE(PushPoint(kBaseTimestamp + i, kBaseUserId + j,
+                            kBaseGpsLongitude, kBaseGpsLatitude,
+                            kBaseGpsAltitude));
+    }
+  }
+
+  for (int j = 0; j < 42; ++j) {
+    proto::GetUserTimelineResponse response;
+    EXPECT_TRUE(FetchTimeline(kBaseUserId + j, &response));
+    EXPECT_EQ(response.point_size(), 10000);
+  }
+
+  EXPECT_TRUE(DeleteUser(kBaseUserId));
+
+  for (int j = 0; j < 42; ++j) {
+    proto::GetUserTimelineResponse response;
+    EXPECT_TRUE(FetchTimeline(kBaseUserId + j, &response));
+
+    if (j == 0) {
+      EXPECT_EQ(response.point_size(), 0);
+    } else {
+      EXPECT_EQ(response.point_size(), 10000);
+    }
+  }
+}
+
 } // namespace
 } // namespace bt
